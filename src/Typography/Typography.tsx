4ca09a523ed52da136eb500a0584208b55c50ab2
james <vdung91@gmail.com>
2019-02-11 17:49:22 +0900

    feat(Typography): Implement default tags

--- src/Typography/Typography.tsx
+++ src/Typography/Typography.tsx
@@ -1,22 +1,69 @@
 import * as React from 'react'
-import { materialize, ClassNameForwardingComponent } from '../Base'
+import { materialize, withDefaultTag, CssProps } from '../Base'
 
-const cssProps = [
-  'display4',
-  'display3',
-  'display2',
-  'display1',
-  'headline',
-  'title',
-  'subheading2',
-  'subheading1',
-  'body2',
+export interface TypographyProps {
+  headline1: boolean
+  headline2: boolean
+  headline3: boolean
+  headline4: boolean
+  headline5: boolean
+  headline6: boolean
+  subtitle1: boolean
+  subtitle2: boolean
+  body1: boolean
+  body2: boolean
+  caption: boolean
+  button: boolean
+  overline: boolean
+}
+
+const cssProps: CssProps<TypographyProps> = [
+  'headline1',
+  'headline2',
+  'headline3',
+  'headline4',
+  'headline5',
+  'headline6',
+  'subtitle1',
+  'subtitle2',
   'body1',
+  'body2',
   'caption',
   'button',
+  'overline',
 ]
 
-export default materialize('mdc-typography', {
+const defaultTags: { [K in keyof TypographyProps]: React.ReactType } = {
+  headline1: 'h1',
+  headline2: 'h2',
+  headline3: 'h3',
+  headline4: 'h4',
+  headline5: 'h5',
+  headline6: 'h6',
+  subtitle1: 'h6',
+  subtitle2: 'h6',
+  body1: 'p',
+  body2: 'p',
+  caption: 'span',
+  button: 'span',
+  overline: 'span',
+}
+
+const Typography: React.FunctionComponent<TypographyProps> = props => {
+  for (const type of cssProps) {
+    if (props[type]) {
+      const { [type]: _, ...others } = props
+      const Tag = defaultTags[type]
+
+      return <Tag {...others} />
+    }
+  }
+
+  return <div {...props} />
+}
+
+export default materialize<TypographyProps>('mdc-typography', {
   cssProps,
+  keepCssProps: cssProps,
   displayName: 'Typography',
-})(ClassNameForwardingComponent)
+})(withDefaultTag(Typography))
