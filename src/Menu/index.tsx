b72c4fd7140de0c50eade4c5692063391ee24759
Cao Viet Dung <vdung91@gmail.com>
2019-02-13 23:40:07 +0900

    fix(Menu): Don't export Corner from Menu to avoid duplicate

--- src/Menu/index.tsx
+++ src/Menu/index.tsx
@@ -2,6 +2,5 @@ import Menu from './Menu'
 import MenuItem from './Item'
 import MenuSelectionGroup from './SelectionGroup'
 import MenuSelectionGroupIcon from './SelectionGroupIcon'
-import Corner from '../MenuSurface/Corner'
 
-export { Menu, MenuItem, MenuSelectionGroup, MenuSelectionGroupIcon, Corner }
+export { Menu, MenuItem, MenuSelectionGroup, MenuSelectionGroupIcon }
